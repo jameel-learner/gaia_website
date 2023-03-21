c51774402b27328a2d84598ebf10f3b1c18a5a17
Mohammed Jameel <jameel.learner@gmail.com>
2023-03-21 06:34:10 +0530

    Major update ... to show IMAGE placeholders ... created new folder assets/sizes/img created new urls in website to show placeholder images

--- website/urls.py
+++ website/urls.py
@@ -3,13 +3,20 @@ from . import views
 app_name = 'website'
 
 urlpatterns = [
-    # path('', views.home, name='home'),
-    # path('contact_us', views.contact_us, name='contact_us'),
-    path('search', views.search, name='search'),
-    path('show_rfq_details/<int:rfq_id>/', views.show_rfq_details, name='show_rfq_details'),
-    # path('about_us', views.about_us, name='about_us'),
-    # path('services/<int:number>/', views.services, name='services'),
-    # path('management', views.management, name='management'),
-    # path('quality', views.quality, name='quality'),
+    path('', views.homepage, name='homepage'),
+    path('contact_us/<slug:service_name>/', views.contact_us, name='contact_us'),
+    path('about_us', views.about_us, name='about_us'),
+    path('history_all', views.history_all, name='history_all'),
+    path('history/<slug:service_name>/', views.history, name='history'),
+    path('strategy', views.strategy, name='strategy'),
+    path('md_message', views.md_message, name='md_message'),
 
+    path('team/<slug:team_name>/', views.team, name='team'),
+    path('business/<slug:service_name>/', views.business, name='business'),
+    path('partnering/<slug:service_name>/', views.partnering, name='partnering'),
+    path('shop', views.shop, name='shop'),
+    path('media_all', views.media_all, name='media_all'),
+    path('media/<slug:mtitle>/', views.media, name='media'),
+    path('shop_all/<slug:category>/', views.shop_all, name='shop_all'),
+    path('product/<int:pid>/<slug:product_name>/', views.product, name='product'),
 ]
